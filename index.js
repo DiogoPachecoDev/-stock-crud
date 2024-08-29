3e9fb28c34db00eb2a6bb56845c2568dbada6529
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-29 20:23:14 -0300

    created and implemented item router

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
+const itemRoute = require('./src/routes/item.routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/items', itemRoute);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
