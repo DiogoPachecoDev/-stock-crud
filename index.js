3372f60ab3c14318f69ff9506a6361e4122c5c78
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-30 12:32:19 -0300

    created middleware to handle route not found error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
+const handle404Error = require('./src/middlewares/handle404Error');
 const itemRoute = require('./src/routes/item.routes');
 const userRoute = require('./src/routes/user.routes');
 
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 app.use('/items', itemRoute);
 app.use('/users', userRoute);
+app.use(handle404Error);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
