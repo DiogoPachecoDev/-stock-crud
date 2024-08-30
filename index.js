070ffacd5c4251e8782d218106c7911d87382d9f
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-30 13:05:28 -0300

    adicionado middleware para tratamento de todos erros do sistema

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
+const handleError = require('./src/middlewares/handleError');
 const handle404Error = require('./src/middlewares/handle404Error');
 const itemRoute = require('./src/routes/item.routes');
 const userRoute = require('./src/routes/user.routes');
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 app.use('/items', itemRoute);
 app.use('/users', userRoute);
 app.use(handle404Error);
+app.use(handleError);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,45 @@
 const userService = require('../services/user.service');
 
-const getAll = async function(req, res) {
-    const userList = await userService.getAll();
-    res.status(200).send(userList);
+const getAll = async function(req, res, next) {
+    try {
+        const response = await userService.getAll();
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(200).send(response);
+    } catch (error) {
+        return next(error);
+    }
 }
 
-const getById = async function(req, res) {
-    const userData = await userService.getById(req.params.id);
-    res.status(200).send(userData);
+const getById = async function(req, res, next) {
+    try {
+        const response = await userService.getById(req.params.id);
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(200).send(response);
+    } catch (error) {
+        return next(error);
+    }
 }
 
-const create = async function(req, res) {
-    const userCreated = await userService.create(req.body);
-    res.status(201).send(userCreated);
+const create = async function(req, res, next) {
+    try {
+        const response = await userService.create(req.body);
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(201).send(response);
+    } catch (error) {
+        return next(error);
+    }
 }
 
 module.exports = {
