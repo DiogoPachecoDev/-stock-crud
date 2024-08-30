aed01e9fe43168222e053e7080a0be207242622e
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-29 21:05:22 -0300

    adjustment of environment variables

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,24 +2,15 @@ require('dotenv').config({ path: __dirname + './../../../.env' });
 
 module.exports = {
     "development": {
-        "username": process.env.DB_DEV_USERNAME,
-        "password": process.env.DB_DEV_PASSWORD,
-        "database": process.env.DB_DEV_DATABASE,
-        "host": process.env.DB_DEV_HOST,
-        "dialect": process.env.DB_DIALECT
-    },
-    "test": {
-        "username": process.env.DB_TEST_USERNAME,
-        "password": process.env.DB_TEST_PASSWORD,
-        "database": process.env.DB_TEST_DATABASE,
-        "host": process.env.DB_TEST_HOST,
-        "dialect": process.env.DB_DIALECT
-    },
-    "production": {
-        "username": process.env.DB_PROD_USERNAME,
-        "password": process.env.DB_PROD_PASSWORD,
-        "database": process.env.DB_PROD_DATABASE,
-        "host": process.env.DB_PROD_HOST,
-        "dialect": process.env.DB_DIALECT
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": process.env.DB_DIALECT,
+        logging: false,
+        timezone: '-03:00',
+        define: {
+            timestamps: true
+        },
     }
 }
