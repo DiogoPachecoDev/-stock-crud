aaeb094ee01a80cf75b90428ed73e9f710e44086
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-29 22:15:13 -0300

    created and implemented user routes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const itemRoute = require('./src/routes/item.routes');
+const userRoute = require('./src/routes/user.routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use('/items', itemRoute);
+app.use('/users', userRoute);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
