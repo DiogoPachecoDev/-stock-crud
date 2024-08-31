35aa19b1023dba847da9d637a4b29f0df12f09c8
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-31 16:18:50 -0300

    created item repository

---
+++ src/repositories/item.repository.js
@@ -0,0 +1,40 @@
+const { Item } = require('../database/models/item');
+
+const getAll = async function() {
+    const itemsList = await Item.findAll();
+    return itemsList;
+}
+
+const getById = async function(id) {
+    const itemData = await Item.findByPk(id);
+    return itemData;
+}
+
+const getByFilter = async function(filter) {
+    const itemData = await Item.findOne({ where: filter });
+    return itemData;
+}
+
+const create = async function(item) {
+    const itemCreated = await Item.create(item);
+    return itemCreated;
+}
+
+const update = async function(item, id) {
+    const itemUpdated = await Item.update(item, { where: { id: id } });
+    return itemUpdated;
+}
+
+const destroy = async function(id) {
+    const itemDestroyed = await Item.destroy({ where: { id: id } });
+    return itemDestroyed;
+}
+
+module.exports = {
+    getAll,
+    getById,
+    getByFilter,
+    create,
+    update,
+    destroy
+}
