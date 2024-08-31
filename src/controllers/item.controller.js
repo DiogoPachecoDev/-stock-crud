5ee9fed2a5b623691c605767c6bfce22105b60b1
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-31 16:45:22 -0300

    created item controller

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -1,7 +1,101 @@
-get = async function(req, res) {
-    res.send({message: 'Hello World'});
+const { validationResult } = require('express-validator');
+const itemService = require('../services/item.service');
+const createErrors = require('http-errors');
+
+const getAll = async function(req, res, next) {
+    try {
+        const response = await itemService.getAll();
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(200).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const getById = async function(req, res, next) {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            throw createErrors(422, { errors: errors.array() })
+        }
+
+        const response = await itemService.getById(req.params.id);
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(200).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const create = async function(req, res, next) {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            throw createErrors(422, { errors: errors.array() })
+        }
+
+        const response = await itemService.create({ name: req.body.name, user_id: req.user_id });
+
+        res.status(201).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const update = async function(req, res, next) {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            throw createErrors(422, { errors: errors.array() })
+        }
+
+        const response = await itemService.update({ name: req.body.name }, req.params.id);
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(204).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const destroy = async function(req, res, next) {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            throw createErrors(422, { errors: errors.array() })
+        }
+
+        const response = await itemService.destroy(req.params.id);
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(202).send(response);
+    } catch (error) {
+        return next(error);
+    }
 }
 
 module.exports = {
-    get
+    getAll,
+    getById,
+    create,
+    update,
+    destroy
 }
