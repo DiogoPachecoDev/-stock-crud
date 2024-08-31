d9b1e374b81d1ba15ccd4e79bba8a61529d5c6a3
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-08-31 16:45:37 -0300

    created item router

--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -1,7 +1,14 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/item.controller');
+const itemValidator = require('../validators/item.validator');
+const handleAuthorization = require('../middlewares/handleAuthorization');
 
-router.get('/', itemController.get);
+router.use(handleAuthorization);
+router.get('/', itemController.getAll);
+router.get('/:id', itemValidator.getById(), itemController.getById);
+router.post('/', itemValidator.create(), itemController.create);
+router.put('/:id', itemValidator.update(), itemController.update);
+router.delete('/:id', itemValidator.destroy(), itemController.destroy);
 
 module.exports = router;
