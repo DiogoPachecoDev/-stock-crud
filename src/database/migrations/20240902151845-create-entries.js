4f845b3c460e81784cc22cc25da099df2587588c
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-09-02 12:50:11 -0300

    created model and migration of entrie

---
+++ src/database/migrations/20240902151845-create-entries.js
@@ -0,0 +1,58 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('entries', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'users'
+          },
+          key: 'id'
+        }
+      },
+      item_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'items'
+          },
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('entries');
+  }
+};
---
+++ src/database/models/entrie.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Entrie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Entrie.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+      Entrie.belongsTo(models.Item, { foreignKey: 'item_id', as: 'item' });
+    }
+  }
+  Entrie.init({
+    quantity: DataTypes.INTEGER,
+    price: DataTypes.DECIMAL(10, 2)
+  }, {
+    sequelize,
+    modelName: 'Entrie',
+    tableName: 'entries',
+    paranoid: true
+  });
+  return Entrie;
+};
