ee789dcad5818a594e1eedd82811b62e75395575
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-09-02 13:26:06 -0300

    created entrie controller

---
+++ src/controllers/entrie.controller.js
@@ -0,0 +1,63 @@
+const { validationResult } = require('express-validator');
+const entrieService = require('../services/entrie.service');
+const createErrors = require('http-errors');
+
+const getAll = async function(req, res, next) {
+    try {
+        const response = await entrieService.getAll();
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(200).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const getById = async function(req, res, next) {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            throw createErrors(422, { errors: errors.array() })
+        }
+
+        const response = await entrieService.getById(req.params.id);
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(200).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+const create = async function(req, res, next) {
+    try {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            throw createErrors(422, { errors: errors.array() })
+        }
+
+        const response = await entrieService.create({ quantity: req.body.quantity, price: req.body.price, item_id: req.body.item_id, user_id: req.user_id });
+
+        if(response && response.message) {
+            throw response;
+        }
+
+        res.status(201).send(response);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create
+}
