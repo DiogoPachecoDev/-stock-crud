c88004a67ed01799d63737916213c965e05c9ec6
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-09-02 13:02:06 -0300

    created entrie validator

---
+++ src/validators/entrie.validator.js
@@ -0,0 +1,21 @@
+const { body, param } = require('express-validator');
+const { validatorMessage } = require('../utils/errorMessage');
+
+const getById = function() {
+    return [
+        param('id', validatorMessage('id')).exists().bail().isInt()
+    ]
+}
+
+const create = function() {
+    return [
+        body('quantity', validatorMessage('quantity')).exists().bail().isInt(),
+        body('price', validatorMessage('price')).exists().bail().isFloat(),
+        body('item_id', validatorMessage('user_id')).exists().bail().isInt()
+    ]
+}
+
+module.exports = {
+    create,
+    getById
+}
