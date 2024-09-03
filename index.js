efbb8892d19896a5bd36470a2509a4431244e7d0
Diogo Pacheco Barcelos <diogopachecopb269@gmail.com>
2024-09-02 21:19:10 -0300

    implemented exit endpoint

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const handle404Error = require('./src/middlewares/handle404Error');
 const itemRoute = require('./src/routes/item.routes');
 const userRoute = require('./src/routes/user.routes');
 const entrieRoute = require('./src/routes/entrie.routes');
+const exitRoute = require('./src/routes/exit.routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use('/items', itemRoute);
 app.use('/users', userRoute);
 app.use('/entries', entrieRoute);
+app.use('/exits', exitRoute);
 
 app.use(handle404Error);
 app.use(handleError);
